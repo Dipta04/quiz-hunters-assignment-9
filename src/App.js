cdbba5680da4968b5c51c442d04d7977b9bc264f
Dipta04 <diptakarmakar38@gmail.com>
2022-10-11 06:58:05 +0600

    blog

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './Layout/Main';
 import Home from './Components/Home/Home';
+import Blog from './Components/Blog/Blog';
 
 function App() {
   const router= createBrowserRouter([
@@ -16,6 +17,10 @@ function App() {
           loader:() =>fetch(' https://openapi.programming-hero.com/api/quiz'),
           element:<Home></Home>
         },
+        {
+          path:'/blog',
+          element:<Blog></Blog>
+        }
       ]
       
     }
